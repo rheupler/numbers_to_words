238bbb72dd01e02a17de6783bf645a88e9d2b431
Julianna Green & Ryan Heupler <student@epicodus.com>
2015-08-04 16:42:42 -0700

    second passing spec

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -2,9 +2,18 @@
 class Fixnum
 
   define_method(:numbers_to_words) do
-      singles = Hash.new()
+
       singles = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five",
       6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 0 => "zero"}
-      singles.fetch(self)
-  end
+
+      teens = {10 => "ten", 11 => "eleven", 12 => "twelve", 13 => "thirteen",
+      14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
+      18 => "eighteen", 19 => "nineteen"}
+
+      if self <= 9
+        singles.fetch(self)
+      else
+        teens.fetch(self)
+      end
+    end
 end
